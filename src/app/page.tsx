6967f1a9bc6fc9f2c4e8b57c7b704c093e9f31af
ปรเมษ แก้วอุบล <keawubolporamet505@gmail.com>
2024-12-28 00:11:19 +0700

    test 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div style={{ position: "relative", width: "100%", height: "100vh" }}>
-      <h1>test</h1>
+      <h1>test222</h1>
       <div style={{ position: "relative", width: "100%", height: "100%" }}>
         <Image src="/toto.jpg" alt="toto" fill style={{ objectFit: "cover" }} />
       </div>
