91d9c9773e43b8c47847c8ddb80a16c6c4d83afd
ปรเมษ แก้วอุบล <keawubolporamet505@gmail.com>
2024-12-27 23:02:59 +0700

    toto test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div style={{ position: "relative", width: "100%", height: "100vh" }}>
+      <div style={{ position: "relative", width: "100%", height: "100%" }}>
+        <Image src="/toto.jpg" alt="toto" fill style={{ objectFit: "cover" }} />
+      </div>
     </div>
   );
 }
